d7ed88035d8e3d0bcbb10386333b572fb459971d
alinddicu <alinddicu@yahoo.com>
2015-02-28 20:56:34 +0100

    testing TestExternalDependencies for written lines

---
+++ ChiFouMi.Test/TestExternalDependenciesTest.cs
@@ -0,0 +1,38 @@
+﻿namespace ChiFouMi.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class TestExternalDependenciesTest
+    {
+        private TestExternalDependencies _testExternalDependecies;
+
+        [TestMethod]
+        public void GivenInputStringsWhenReadLineThenReturnCorrectStrings()
+        {
+            var list = new List<string>{"1", "2"};
+            list.Reverse();
+            _testExternalDependecies = new TestExternalDependencies(list);
+
+            Check.That(_testExternalDependecies.ReadLine()).IsEqualTo("1");
+            Check.That(_testExternalDependecies.ReadLine()).IsEqualTo("2");
+        }
+
+        [TestMethod]
+        public void GivenLineToWriteWhenGetWrittenLinesThenWrittenLinesAreCorrect()
+        {
+            _testExternalDependecies = new TestExternalDependencies(new List<string>());
+
+            _testExternalDependecies.WriteLine("nouvelle ligne");
+
+            var writtenLines = _testExternalDependecies.GetWrittenLines().ToArray();
+            Check.That(writtenLines).HasSize(2);
+            Check.That(writtenLines[0]).IsEqualTo("nouvelle ligne");
+            Check.That(writtenLines[1]).IsEqualTo(Environment.NewLine);
+        }
+    }
+}
---
+++ ChiFouMi/TestExternalDependencies.cs
@@ -0,0 +1,32 @@
+﻿namespace ChiFouMi
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class TestExternalDependencies : IExternalDependecies
+    {
+        private readonly List<string> _linesBuffer = new List<string>();
+        private readonly Stack<string> _inputLines;
+
+        public TestExternalDependencies(IEnumerable<string> inputLines)
+        {
+            _inputLines = new Stack<string>(inputLines);
+        }
+
+        public void WriteLine(string line)
+        {
+            _linesBuffer.Add(line);
+            _linesBuffer.Add(Environment.NewLine);
+        }
+
+        public IEnumerable<string> GetWrittenLines()
+        {
+            return _linesBuffer;
+        }
+
+        public string ReadLine()
+        {
+            return _inputLines.Pop();
+        }
+    }
+}
