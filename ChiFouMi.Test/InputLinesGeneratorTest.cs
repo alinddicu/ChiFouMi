099b08b6c912202aef551fc5a186ed5f82fe6a2f
alinddicu <alinddicu@yahoo.com>
2015-02-28 23:00:32 +0100

    InputLinesGenerator avec TU

---
+++ ChiFouMi.Test/InputLinesGeneratorTest.cs
@@ -0,0 +1,38 @@
+﻿namespace ChiFouMi.Test
+{
+    using System;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class InputLinesGeneratorTest
+    {
+        private InputLinesGenerator _generator;
+
+        [TestMethod]
+        public void CheckThatInputLinesAreGeneratedCorrectly()
+        {
+            _generator = new InputLinesGenerator();
+            const int linesNumber = 10;
+
+            var lines = _generator.Generate(linesNumber).ToArray();
+
+            Check.That(EnumerableExtensions.Count(lines)).IsEqualTo(2 + linesNumber * 2);
+            Check.That(lines.Last()).IsEqualTo("exit");
+            Check.That(lines[lines.Count()-2]).IsEqualTo(Environment.NewLine);
+            for (var i = 0; i < linesNumber * 2; i++)
+            {
+                var lineValue = lines[i];
+                if (i % 2 == 1)
+                {
+                    Check.That(Convert.ToInt32(lineValue)).IsGreaterThan(0).And.IsLessThan(4);
+                }
+                else
+                {
+                    Check.That(lineValue).IsEqualTo(Environment.NewLine);
+                }
+            }
+        }
+    }
+}
---
+++ ChiFouMi/InputLinesGenerator.cs
@@ -0,0 +1,23 @@
+﻿namespace ChiFouMi
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    public class InputLinesGenerator
+    {
+        private readonly Random _randomGenerator = new Random(DateTime.Now.Millisecond);
+
+        public IEnumerable<string> Generate(int linesNumber)
+        {
+            for (var i = 0; i < linesNumber; i++)
+            {
+                yield return Environment.NewLine;
+                yield return _randomGenerator.Next(1, 4).ToString(CultureInfo.InvariantCulture);
+            }
+
+            yield return Environment.NewLine;
+            yield return "exit";
+        }
+    }
+}
