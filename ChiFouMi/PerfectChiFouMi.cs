eb9ba76bddf548e5b5fb9603d348f28be227ad69
alinddicu <alinddicu@yahoo.com>
2015-03-02 23:39:41 +0100

    basic style

--- ChiFouMi/PerfectChiFouMi.cs
+++ ChiFouMi/PerfectChiFouMi.cs
@@ -1,54 +1,56 @@
 ﻿namespace ChiFouMi
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
 
     public class PerfectChiFouMi : IChiFouMi
     {
-        public static int _a0;
-        private static char _intUs;
-        private static Random r;
-        public static int _intUv;
-        public static int cnt = 0;
-        private static Stack<string> t = new Stack<string>();
-        private static bool rmdi = false;
-        public static string str_end = "exit";
+        private const string ExitPhrase = "exit";
+        private const string TextIntro = "Veuillez choisir un signe:";
+
+        private int _intUs;
+        private int _intUv;
+
+        private readonly Stack<string> t = new Stack<string>();
+
+        private bool _roxorMode;
 
         private readonly IExternalDependecies _dependencies;
 
         public PerfectChiFouMi(IExternalDependecies dependencies)
         {
             _dependencies = dependencies;
+            t.Push("Ciseaux");
+            t.Push("Feuille");
+            t.Push("Pierre");
         }
 
         public void Play(string[] args)
         {
-            _a0 = 0;
             if (args.Any())
             {
-                if (args[_a0].Equals("roxor")) roxorMoMode = true;
+                if (args[0].Equals("roxor"))
+                {
+                    _roxorMode = true;
+                }
             }
 
-            _str = "exit";
-            _strTextIntro = "Veuillez choisir un signe:";
             _dependencies.WriteLine("Bienvenue dans mon chifumi," +
                               " ici c'est un appli de ROXXXXXXXXXXXXXXXOOR!");
             _dependencies.WriteLine("Taper sur la touche entrée pour commencer une partie," +
                               " ou 'exit' pour quitter.");
             while (!Initialize())
             {
-                _dependencies.WriteLine(_strTextIntro);
+                _dependencies.WriteLine(TextIntro);
                 for (var idChoix = 0; idChoix < t.Count; idChoix++)
                 {
                     Display(idChoix);
                 }
                 _intUs = (char)(_dependencies.ReadLine()[0] - 48);
 
-                r = new Random(DateTime.Now.Millisecond);
                 _intUv = (char)(_dependencies.GetNextRandomBetween1And3().ToString()[0] - 48);
 
-                if (roxorMoMode != rmdi && _intUv == 1)
+                if (_roxorMode && _intUv == 1)
                 {
                     _dependencies.WriteLine("Tu es un roxor contre Pierre");
                     _dependencies.WriteLine("Gagne!");
@@ -58,7 +60,7 @@
                     _dependencies.WriteLine("Pierre contre Pierre!");
                     _dependencies.WriteLine("Egalite!");
                 }
-                else if (roxorMoMode != rmdi && _intUv == 2)
+                else if (_roxorMode && _intUv == 2)
                 {
                     _dependencies.WriteLine("Tu es un roxor contre Feuille");
                     _dependencies.WriteLine("Gagne!");
@@ -68,14 +70,14 @@
                     _dependencies.WriteLine("Pierre contre Feuille!");
                     _dependencies.WriteLine("Perdu!");
                 }
-                else if (roxorMoMode != rmdi && _intUv == 3)
+                else if (_roxorMode && _intUv == 3)
                 {
                     _dependencies.WriteLine("Tu es un roxor contre Ciseaux");
                     _dependencies.WriteLine("Gagne!");
                 }
                 else if (_intUs == 1 && _intUv == 3)
                 {
-                    _dependencies.WriteLine("Pierre contre Ciseaux!"); 
+                    _dependencies.WriteLine("Pierre contre Ciseaux!");
                     _dependencies.WriteLine("Gagne!");
                 }
                 else if (_intUs == 2 && _intUv == 2)
@@ -98,25 +100,16 @@
                     _dependencies.WriteLine("Ciseaux contre Ciseaux!");
                     _dependencies.WriteLine("Egalite!");
                 }
-                else if ("exit".Equals(_str))
+                else
                 {
                     break;
                 }
             }
         }
 
-        private static bool roxorMoMode;
-        private static string _str;
-        private static string _strTextIntro;
-
         private bool Initialize()
         {
-            t = new Stack<string>();
-            cnt = _intUv;
-            t.Push("Ciseaux");
-            t.Push("Feuille");
-            t.Push("Pierre");
-            return _dependencies.ReadLine().StartsWith(str_end);
+            return _dependencies.ReadLine().StartsWith(ExitPhrase);
         }
 
         private void Display(int idChoix)
